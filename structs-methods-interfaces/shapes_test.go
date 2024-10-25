2f10815d22b51a7262269cd29ae08e7f85ea944a
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2024-10-26 00:26:47 +0100

    Refactor: Created rectangle class to better represent rectangles

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -3,14 +3,16 @@ package structs_methods_interfaces
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	assertEquals(t, got, want)
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
 	want := 72.0
 
 	assertEquals(t, got, want)
