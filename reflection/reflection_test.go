8159b2db8b8696f184244bf11ab6f7da7d0943d3
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2024-10-26 14:03:14 +0100

    Feat: Improved walk function to provide correct output

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -4,18 +4,37 @@ import "testing"
 
 func TestWalk(t *testing.T) {
 
-	expected := "Chris"
-	var got []string
+	t.Run("test length", func(t *testing.T) {
+		expected := "Chris"
+		var got []string
 
-	x := struct {
-		Name string
-	}{expected}
+		x := struct {
+			Name string
+		}{expected}
 
-	walk(x, func(input string) {
-		got = append(got, input)
+		walk(x, func(input string) {
+			got = append(got, input)
+		})
+
+		if len(got) != 1 {
+			t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+		}
 	})
 
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
-	}
+	t.Run("test output", func(t *testing.T) {
+		expected := "Chris"
+		var got []string
+
+		x := struct {
+			Name string
+		}{expected}
+
+		walk(x, func(input string) {
+			got = append(got, input)
+		})
+
+		if got[0] != expected {
+			t.Errorf("got %q want %q", got[0], expected)
+		}
+	})
 }
