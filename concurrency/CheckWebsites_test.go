94536f114ae6d9ec03db1085f7f7bc31ecbb94d4
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2024-10-26 12:23:22 +0100

    Feat: Added concurrency support to increase performance

--- concurrency/CheckWebsites_test.go
+++ concurrency/CheckWebsites_test.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"maps"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
@@ -26,5 +27,21 @@ func TestCheckWebsites(t *testing.T) {
 
 	if !maps.Equal(want, got) {
 		t.Fatalf("wanted %v got %v", want, got)
+	}
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := range urls {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
